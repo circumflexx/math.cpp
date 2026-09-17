090519a35440a4598bdbab04709fca2b48a6ed95
^_^ <geniidoti2@icloud.com>
2026-09-17 21:43:46 +0300

    Update absolute.cpp

--- algebra/absolute.cpp
+++ algebra/absolute.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 
-void absolute(int& n) {
-    if (n < 0) n = -n;
+int absolute(int n) {
+    return (n < 0) ? -n : n;
+}
+double absolute(double n) {
+    return (n < 0) ? -n : n;
 }
