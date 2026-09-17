ef1dbef637e20669e2bd178a45fa0b29bb8be9c4
^_^ <geniidoti2@icloud.com>
2026-09-17 22:04:45 +0300

    Update arithmeticmean.cpp

--- algebra/arithmeticmean.cpp
+++ algebra/arithmeticmean.cpp
@@ -1,9 +1,10 @@
-#include <iostream>
-using namespace std;
-
-int arithmeticmean() {
-  
-}
-double arithmeticmean() {
-  
+#include <vector>
+template <typename t>
+double arithmeticmean(const std::vector<t>& m) {
+    if (m.empty()) return 0.0;
+    double s = 0.0;
+    for (t x : m) {
+        s += x;
+    }
+    return s / m.size();
 }
