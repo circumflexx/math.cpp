a69a138b82be07b34cd191eea154753facc7dea0
^_^ <geniidoti2@icloud.com>
2026-09-18 15:14:33 +0300

    Delete algebra/arithmetic/aprogressionterm2.cpp

--- algebra/arithmetic/aprogressionterm2.cpp
+++
@@ -1,6 +0,0 @@
-template <typename t>
-auto aprogressionterm2(t a1, t a2, int n) {
-    if (n <= 0) return 0.0;
-    double d = static_cast<double>(a2) - static_cast<double>(a1);
-    return static_cast<double>(a1) + (n - 1) * d;
-}
