aa9d70f0dccf3a0017c46020188686eba5f0f3c0
^_^ <geniidoti2@icloud.com>
2026-09-18 15:14:44 +0300

    Delete algebra/arithmetic/aprogressionsum2.cpp

--- algebra/arithmetic/aprogressionsum2.cpp
+++
@@ -1,5 +0,0 @@
-template <typename t>
-auto aprogressionsum2(t a1, t d, int n) {
-    if (n <= 0) return 0.0;
-    return static_cast<double>(n) * (2.0 * static_cast<double>(a1) + (n - 1) * static_cast<double>(d)) / 2.0;
-}
