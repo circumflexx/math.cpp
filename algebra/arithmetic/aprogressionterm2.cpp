9e273417466bf99ca3fecc4a942fa816a415de2a
^_^ <geniidoti2@icloud.com>
2026-09-18 14:34:34 +0300

    Update aprogressionterm2.cpp

--- algebra/arithmetic/aprogressionterm2.cpp
+++ algebra/arithmetic/aprogressionterm2.cpp
@@ -1,5 +1,5 @@
 template <typename t>
-auto aprogressionterm(t a1, t a2, int n) {
+auto aprogressionterm2(t a1, t a2, int n) {
     if (n <= 0) return 0.0;
     double d = static_cast<double>(a2) - static_cast<double>(a1);
     return static_cast<double>(a1) + (n - 1) * d;
