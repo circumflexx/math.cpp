73b14c95046c0533f9f68ebfc9ca1eb1a801f957
^_^ <geniidoti2@icloud.com>
2026-09-18 14:55:12 +0300

    Update aprogressionsum2.cpp

--- algebra/arithmetic/aprogressionsum2.cpp
+++ algebra/arithmetic/aprogressionsum2.cpp
@@ -1,1 +1,5 @@
-
+template <typename t>
+auto aprogressionsum2(t a1, t d, int n) {
+    if (n <= 0) return 0.0;
+    return static_cast<double>(n) * (2.0 * static_cast<double>(a1) + (n - 1) * static_cast<double>(d)) / 2.0;
+}
