01197cceab79777d1ae5b3d4e8ef218748f83f39
^_^ <geniidoti2@icloud.com>
2026-09-18 15:11:47 +0300

    Update arithmeticmean.cpp

--- algebra/arithmetic/arithmeticmean.cpp
+++ algebra/arithmetic/arithmeticmean.cpp
@@ -1,9 +1,9 @@
 #include <vector>
-template <typename t>
-auto arithmeticmean(const std::vector<t>& m) {
+template <typename T>
+auto arithmeticmean(const std::vector<T>& m) {
     if (m.empty()) return double{0};
     double s = 0;
-    for (const t& x : m) {
+    for (const T& x : m) {
         s += x;
     }
     return s / static_cast<double>(m.size());
